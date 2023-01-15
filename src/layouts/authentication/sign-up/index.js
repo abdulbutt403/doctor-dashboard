9baef2804a11e25381fbcbb3e89f389781ad3906
abdulbutt403 <75443479+abdulbutt403@users.noreply.github.com>
2023-01-15 11:59:24 -0800

    phoneNumber sign up

--- src/layouts/authentication/sign-up/index.js
+++ src/layouts/authentication/sign-up/index.js
@@ -17,6 +17,7 @@ function Cover() {
   var [email, setEmail] = useState("admin@pharmacist.com");
   var [fullName, setfullName] = useState("admin@pharmacist.com");
   var [password, setPassword] = useState("");
+  var [phoneNumber, setPhoneNumber] = useState("");
   var [role, setRole] = useState("DOCTOR");
   const [checked, setChecked] = React.useState(true);
   const [terms, setTerms] = React.useState(false);
@@ -38,13 +39,14 @@ function Cover() {
       toast.error(`Please Enter a valid email..!`);
     } else if (!checked) {
       toast.error(`Please Agree to terms and conditions`);
-    } else if (!!login && !!password && !!role && checked) {
+    } else if (!!login && !!password && !!role && checked && phoneNumber) {
       try {
         const res = await axios.post(endPoint + "/users/create", {
           email: login,
           password: password,
           role: role,
           fullName: fullName,
+          phoneNumber: phoneNumber
         });
         if (res.data.success) {
           toast.success("A verification code is sent to your email");
@@ -129,6 +131,16 @@ function Cover() {
                   fullWidth
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
+                />
+              </MDBox>
+              <MDBox mb={2}>
+                <MDInput
+                  type="text"
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                  label="Phone (with country code e.g +92)"
+                  variant="standard"
+                  fullWidth
                 />
               </MDBox>
               <MDBox display="flex" alignItems="center" ml={-1}>
